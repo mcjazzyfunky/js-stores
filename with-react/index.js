209a48d285d26eb73a1b5ddfb3eb875033886ef9
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2019-03-27 12:18:51 +0100

    Some fixes

--- with-react/index.js
+++ with-react/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('../dist/js-stores.react.cjs.production.js')
+  module.exports = require('../dist/js-stores.with-react.cjs.production.js')
 } else {
-  module.exports = require('../dist/js-stores.react.cjs.development.js')
+  module.exports = require('../dist/js-stores.with-react.cjs.development.js')
 }
