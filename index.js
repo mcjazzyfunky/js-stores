37a67a1cb799eedcf2003d877108c4c7c05b9153
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2019-09-29 18:47:46 +0200

    Some updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/js-stores.core.cjs.production.js')
+  module.exports = require('./dist/js-stores.cjs.production.js')
 } else {
-  module.exports = require('./dist/js-stores.core.cjs.development.js')
+  module.exports = require('./dist/js-stores.cjs.development.js')
 }
