82a31441f95aaa114d3921acd6f54528425ffe55
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-05-16 15:06:36 +0200

    Fixed type of Store

--- src/main/internal/types/Store.ts
+++ src/main/internal/types/Store.ts
@@ -2,6 +2,7 @@ import State from './State'
 import Message from './Message'
 
 type Store<S extends State, M extends Message> = {
+  dispatch(msg: M): void,
   getState(): S,
   subscribe(listener: () => void): () => void,
 }
