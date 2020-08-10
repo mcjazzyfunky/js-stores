1f58d752a79521ff17da864e4bdc138cb8d31899
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-08-10 05:11:03 +0200

    Some updates

--- src/main/api/combineEffects.ts
+++ src/main/api/combineEffects.ts
@@ -3,7 +3,7 @@ import Effect from './types/Effect'
 import Effects from './types/Effects'
 import State from './types/State'
 
-export function combineEffects<S extends State>(
+export default function combineEffects<S extends State>(
   ...args: (Effect<S> | Effects<S> | Iterable<Effect<S>>)[]
 ): Effect<S> {
   const effects: Effect<S>[] = []
@@ -12,14 +12,10 @@ export function combineEffects<S extends State>(
     const eff = args[i]
     const type = typeof eff
 
-    if (!eff || (type !== 'function' && type !== 'object')) {
-      return
-    }
-
     if (typeof eff === 'function') {
       effects.push(eff)
-    } else if (typeof eff[Symbol.iterator] === 'function') {
-      for (const eff2 of eff as Iterable<Effect<S>>) {
+    } else if (typeof (eff as any)[Symbol.iterator] === 'function') {
+      for (const eff2 of Array.from(eff as any) as any) {
         effects.push(eff2)
       }
     } else {
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,8 @@
+// functions
 export { default as createStore } from './api/createStore'
+export { default as combineEffects } from './api/combineEffects'
+
+// types
+export { default as Effect } from './api/types/Effect'
+export { default as Effects } from './api/types/Effects'
+export { default as State } from './api/types/State'
